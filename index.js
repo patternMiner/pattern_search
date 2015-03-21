5cb0b049a93043dd3022d3e6d3d23b8b692601c0
patternMiner <jbisa61@gmail.com>
2015-03-21 14:57:55 -0400

    Simplify index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import {bootstrap} from 'angular2/angular2';
 import {PatternSearch} from 'pattern_search/components/pattern_search';
 
-export function main() {
-  bootstrap(PatternSearch);
-}
+bootstrap(PatternSearch);
